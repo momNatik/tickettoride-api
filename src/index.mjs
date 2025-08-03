623964fe27a1f3e0710d1ca1c933f15732a8f996
Nataliya <mom.natik@gmail.com>
2025-08-03 18:10:49 +0300

    cleanup

--- src/index.mjs
+++ src/index.mjs
@@ -1,21 +1,14 @@
 import express from "express";
 import cors from "cors";
-import { RegisterGameResourcesEndpoints } from "./js/game-resources-router.mjs";
-import { initGameAsync } from "./js/game.mjs";
-import path from "path";
-import fs from "node:fs/promises";
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-const port = process.env.PORT;
-
-const corsOptions = {
-  origin: ["http://localhost:3000"],
-};
+const corsOptions = { origin: [process.env.WEB_URL] };
 
 const app = express();
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
-RegisterGameResourcesEndpoints(app);
+app.use('/games', createProxyMiddleware({ target: process.env.GAME_API_URL, changeOrigin: true }));
 
-app.listen(port, () => console.log("Game API waiting for connections..."));
+app.listen(process.env.PORT, () => console.log("Game API waiting for connections..."));
--- src/js/resources/router.mjs
+++
@@ -1,6 +0,0 @@
-import { GetGameStatus, GetMapBackgroundImage } from "./game-resources.mjs";
-
-export function RegisterGameResourcesEndpoints(app) {
-    app.get("/:gameId/resources/status", GetGameStatus);
-    app.get("/:gameId/resources/mapbackgroundimage", GetMapBackgroundImage);
-}
