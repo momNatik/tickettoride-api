ade65a0722e9c0ebff6cbda776f92c430a511c92
Nataliya <mom.natik@gmail.com>
2025-08-22 16:49:07 +0300

    + submodule

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const corsOptions = { origin: [process.env.WEB_URL] };
 
@@ -9,6 +9,32 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use('/games', createProxyMiddleware({ target: process.env.GAME_API_URL, changeOrigin: true }));
+app.use(Log);
 
-app.listen(process.env.API_PORT, () => console.log("API waiting for connections..."));
+app.use(
+  "/games",
+  createProxyMiddleware({
+    target: process.env.GAME_API_URL,
+    changeOrigin: true,
+  })
+);
+
+app.listen(process.env.API_PORT, () =>
+  console.log("API waiting for connections...")
+);
+
+function Log(req, res, next) {
+  const now = new Date(Date.now());
+  const timeString = ToPreciseTime(now);
+  const message = `${req.path}`;
+
+  console.log(
+    `${timeString}: \x1b[33m${message}\x1b[0m`
+  );
+
+  next();
+}
+
+function ToPreciseTime(date) {
+  return date.toISOString().substring(11, 23);
+}
